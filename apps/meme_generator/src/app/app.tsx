d76e26044bc20b4af63a6e2eb50a893003f73e1e
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-05-29 15:14:26 +0300

    Create meme_generator app with vite and Nx

---
+++ apps/meme_generator/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@learning-react/meme_generator" />
+    </div>
+  );
+}
+
+export default App;
