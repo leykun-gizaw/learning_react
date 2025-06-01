8d6a7ef357615cc1701697595145908858226754
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-01 10:35:58 +0300

    Create Tenzies app

---
+++ apps/Tenzies/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@learning-react/Tenzies" />
+    </div>
+  );
+}
+
+export default App;
