c649fbe72a5c9be81ce2d4f41793e7c2578e8cb3
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-02 22:33:15 +0300

    Implement game logic and celebrate

--- apps/Tenzies/src/app/components/DiceContainer.tsx
+++ apps/Tenzies/src/app/components/DiceContainer.tsx
@@ -1,28 +1,61 @@
 import { useState } from 'react';
 import Die from './Die';
+import { nanoid } from 'nanoid';
+import ReactConfetti from 'react-confetti';
 
 const DiceContainer = () => {
-  const generateAllNewDice = () => {
+  function generateAllNewDice() {
     return Array.from({ length: 10 }, () => ({
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
+      id: nanoid(),
     }));
-  };
+  }
+  function handleDieClick(id: string) {
+    setDiceValues((prevDice) =>
+      prevDice.map((die) =>
+        id === die.id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+  function handleDiceRole() {
+    if (gameWon) {
+      setDiceValues(generateAllNewDice());
+      return;
+    }
+    setDiceValues((prevDice) =>
+      prevDice.map((die) =>
+        die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+      )
+    );
+  }
   const [diceValues, setDiceValues] = useState(() => generateAllNewDice());
+
   const DiceElements = diceValues.map((dieObj, index) => (
-    <Die key={index} value={dieObj.value} />
+    <Die key={dieObj.id} {...dieObj} handleDieClick={handleDieClick} />
   ));
+  const gameWon = diceValues.every(
+    (die) => die.isHeld && die.value === diceValues[0].value
+  );
 
   return (
     <div className="w-full m-auto h-full flex flex-col justify-evenly items-center">
+      {gameWon && <ReactConfetti />}
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="text-4xl font-extrabold">Tenzies</h1>
+        <p className="text-center font-semibold">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
       <div className="dice-container grid grid-cols-5 grid-rows-2 w-9/12 h-40 gap-5">
         {DiceElements}
       </div>
       <button
-        onClick={() => setDiceValues(generateAllNewDice())}
+        onClick={handleDiceRole}
         className="bg-[rgb(80,53,255)] text-white px-5 py-3 rounded text-xl font-bold"
       >
-        Roll Dice
+        {gameWon ? 'New Game' : 'Roll Dice'}
       </button>
     </div>
   );
--- apps/Tenzies/src/app/components/Die.tsx
+++ apps/Tenzies/src/app/components/Die.tsx
@@ -1,6 +1,21 @@
-const Die = ({ value }: { value: number }) => {
+const Die = ({
+  id,
+  value,
+  isHeld,
+  handleDieClick,
+}: {
+  id: string;
+  value: number;
+  isHeld: boolean;
+  handleDieClick: (id: string) => void;
+}) => {
+  const dieStyles =
+    'die border border-slate-300 rounded-md shadow-[0px_2px_2px_rgba(0,0,0,0.25)] active:shadow-none';
   return (
-    <button className="die border border-slate-300 rounded-md shadow-[0px_2px_2px_rgba(0,0,0,0.25)] active:shadow-none bg-white">
+    <button
+      className={dieStyles + (isHeld ? ' bg-[#59E391]' : ' bg-white')}
+      onClick={() => handleDieClick(id)}
+    >
       <span className="font-bold text-2xl">{value}</span>
     </button>
   );
