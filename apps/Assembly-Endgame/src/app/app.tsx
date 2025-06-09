db4e39449a6c71ee42406fa5e295a7068c27030f
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-09 19:49:09 +0300

    feat(Assembly-Endgame): Add the title section

--- apps/Assembly-Endgame/src/app/app.tsx
+++ apps/Assembly-Endgame/src/app/app.tsx
@@ -1,9 +1,24 @@
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <div>
-      <NxWelcome title="@learning-react/Assembly-Endgame" />
+    <div className="bg-[rgb(40,39,38)] h-screen text-white flex flex-col items-center">
+      <section className="title text-center mt-40 flex flex-col gap-2 max-w-96">
+        <h1 className="text-2xl text-[rgb(249,244,218)]">Assembly: Endgame</h1>
+        <p className="text-[rgb(135,135,135)] max-w-[400px] text-lg">
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </section>
+      <section className="mt-10 h-[60px] bg-[rgb(21,169,90)] w-[500px] rounded-lg flex flex-col justify-center items-center">
+        <h2 className="text-center text-[rgb(249,244,218)] text-xl">
+          You Won!
+        </h2>
+        <p className="text-center">
+          Well done{' '}
+          <span role="img" aria-label="Party popper emoji">
+            🎉
+          </span>
+        </p>
+      </section>
     </div>
   );
 }
