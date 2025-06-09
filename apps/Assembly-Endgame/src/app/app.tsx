1df6be5192aeab4fc47e17e93d719ee493481bac
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-10 01:53:02 +0300

    feat(Assembly-Endgame: Update language elements to show death when wrong

--- apps/Assembly-Endgame/src/app/app.tsx
+++ apps/Assembly-Endgame/src/app/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { type JSX, useState } from 'react';
 import { languages } from '../utils/constants';
 import { generateKeyboard } from '../utils/helpers';
 import clsx from 'clsx';
@@ -6,20 +6,66 @@ import clsx from 'clsx';
 export function App() {
   const [keyboard, setKeyboard] = useState(() => generateKeyboard());
 
-  const letterElements = keyboard
-    .filter((key) => key.foundAt.length > 0)
-    .map((key) => key.foundAt)
-    .flat(1)
-    .map((index) => (
+  const wrongAttempts = keyboard
+    .filter((key) => key.foundAt.length === 0)
+    .reduce((acc, key) => acc + (key.clicked ? 1 : 0), 0);
+  console.log('Wrong attempts:', wrongAttempts);
+
+  const LanguageElements = languages.map((language, idx) => {
+    const classNames = clsx({
+      'text-md': true,
+      'rounded-[3px]': true,
+      'p-1': true,
+      relative: true,
+      'before:absolute': idx < wrongAttempts,
+      'before:content-["💀"]': idx < wrongAttempts,
+      'before:z-10': idx < wrongAttempts,
+      'before:inset-0': idx < wrongAttempts,
+      'before:bg-black': idx < wrongAttempts,
+      'before:opacity-70': idx < wrongAttempts,
+      'before:flex': idx < wrongAttempts,
+      'before:items-center': idx < wrongAttempts,
+      'before:justify-center': idx < wrongAttempts,
+    });
+    return (
       <span
-        key={index}
-        className="bg-[rgb(50,50,50)] h-12 w-12 border-b flex justify-center items-center text-3xl font-extralight"
-      ></span>
-    ));
+        className={classNames}
+        key={language.name}
+        style={{
+          backgroundColor: language.backgroundColor,
+          color: language.color,
+        }}
+      >
+        {language.name}
+      </span>
+    );
+  });
+
+  // I resisted the urge to use a for loop to insert letters into spans
+  // using innerText.
+  const letterElements: JSX.Element[] = Array(
+    keyboard
+      .filter((key) => key.foundAt.length > 0)
+      .reduce((acc, key) => acc + key.foundAt.length, 0)
+  );
+  keyboard
+    .filter((keyObj) => keyObj.foundAt.length > 0)
+    .forEach((keyObj) => {
+      keyObj.foundAt.forEach((index) => {
+        letterElements[index] = (
+          <span
+            key={index}
+            className="bg-[rgb(50,50,50)] h-12 w-12 border-b flex justify-center items-center text-3xl font-extralight"
+          >
+            {keyObj.clicked ? keyObj.ltr : ''}
+          </span>
+        );
+      });
+    });
 
   const keyboardElements = keyboard.map((key) => {
     const keyStatusClass = clsx({
-      'bg-[rgb(252,186,41)]': true,
+      'bg-[rgb(252,186,41)]': !key.clicked,
       'text-[rgb(30,30,30)]': true,
       'font-semibold': true,
       'text-xl': true,
@@ -64,19 +110,8 @@ export function App() {
           </span>
         </p>
       </section>
-      <section className="mt-10 flex flex-wrap gap-[2px] justify-center w-[350px]">
-        {languages.map((language) => (
-          <span
-            className="text-md rounded-[3px] p-1"
-            key={language.name}
-            style={{
-              backgroundColor: language.backgroundColor,
-              color: language.color,
-            }}
-          >
-            {language.name}
-          </span>
-        ))}
+      <section className="mt-10 flex flex-wrap gap-[2px] justify-center w-[350px] relative">
+        {LanguageElements}
       </section>
       <section className="mt-10 w-[500px] flex justify-center items-center gap-1">
         {letterElements}
