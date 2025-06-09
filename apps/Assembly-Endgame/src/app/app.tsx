8790cd5d3a62cb9bd860bb66e5de622e5caf95d1
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-09 19:05:48 +0300

    Add assembly game app

---
+++ apps/Assembly-Endgame/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@learning-react/Assembly-Endgame" />
+    </div>
+  );
+}
+
+export default App;
