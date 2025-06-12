37e452d42202e4cc715f88781506709af112a7b6
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-06-12 17:40:49 +0300

    Create a new app called quizzical

---
+++ apps/quizzical/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@learning-react/quizzical" />
+    </div>
+  );
+}
+
+export default App;
