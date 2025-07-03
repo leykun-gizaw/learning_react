92ee717c189b6cb48dcfa764cb62b707a96f5246
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-07-03 13:27:29 +0300

    refactor(App): Enhance layout and functionality with improved button styling and product visibility toggle

--- apps/lazy-suspense/src/app/app.tsx
+++ apps/lazy-suspense/src/app/app.tsx
@@ -9,18 +9,33 @@ export function App() {
   const decrement = () => setCount(count - 1);
 
   return (
-    <div className="border text-cyan-900 text-center">
-      <h1>The current count is {count}</h1>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
-      <br />
-      <br />
-      <button onClick={() => setShowProducts((prev) => !prev)}>
-        {showProducts ? 'Hide' : 'Show'} Products
-      </button>
-      <br />
-      <br />
-      <div>{showProducts && <ProductsList />}</div>
+    <div className="flex flex-col gap-5 p-5 border text-cyan-900 w-1/2 m-5 ">
+      <div className="flex gap-5 justify-center border p-5">
+        <h1 className="text-center">The current count is {count}</h1>
+        <div>
+          <button
+            className="w-[50px] border hover:bg-cyan-200 active:bg-cyan-100 rounded"
+            onClick={decrement}
+          >
+            -
+          </button>
+          <button
+            className="w-[50px] border hover:bg-cyan-200 active:bg-cyan-100 rounded"
+            onClick={increment}
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-5 p-5 border">
+        <button
+          className="w-[250px] border hover:bg-cyan-200 active:bg-cyan-100 rounded"
+          onClick={() => setShowProducts((prev) => !prev)}
+        >
+          {showProducts ? 'Hide' : 'Show'} Products
+        </button>
+        <div>{showProducts && <ProductsList />}</div>
+      </div>
     </div>
   );
 }
