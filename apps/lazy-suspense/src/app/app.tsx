6e97458b803ac7bfc34f4d2973a7a53ebcbc83c5
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-07-03 13:16:33 +0300

    feat(App): Refactor App component to include product count and toggle product list visibility

--- apps/lazy-suspense/src/app/app.tsx
+++ apps/lazy-suspense/src/app/app.tsx
@@ -1,7 +1,26 @@
+import React from 'react';
+import ProductsList from './ProductList';
+
 export function App() {
+  const [count, setCount] = React.useState(0);
+  const [showProducts, setShowProducts] = React.useState(false);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
-    <div className="border text-cyan-900">
-      <h1 className="text-3xl text-center">Code Splitting, lazy, Suspense</h1>
+    <div className="border text-cyan-900 text-center">
+      <h1>The current count is {count}</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+      <br />
+      <br />
+      <button onClick={() => setShowProducts((prev) => !prev)}>
+        {showProducts ? 'Hide' : 'Show'} Products
+      </button>
+      <br />
+      <br />
+      <div>{showProducts && <ProductsList />}</div>
     </div>
   );
 }
