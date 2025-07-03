6b1577bcd65c56ddbcaf69acafa96ce894c4b0a7
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-07-03 12:40:17 +0300

    feat(learning_react): Add a new app to learn ...
    code spliting, lazy, & suspense

---
+++ apps/lazy-suspense/src/app/app.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="border text-cyan-900">
+      <h1 className="text-3xl text-center">Code Splitting, lazy, Suspense</h1>
+    </div>
+  );
+}
+
+export default App;
